de1c8929e0c761943ba2fa63dd4748b32f3ba384
anurag5764 <danurag983@gmail.com>
2025-06-27 18:31:40 +0530

    : AuthOptions added

--- web/app/api/auth/[...nextauth]/route.ts
+++ web/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
-import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
-
-export const { handlers: { GET, POST } } = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-});
+import NextAuth from "next-auth";
+import { AuthOptions } from "next-auth";
+import GitHub from "next-auth/providers/github";
+export const authOptions: AuthOptions = {
+  providers: [GitHub({
+    clientId: process.env.GITHUB_ID!,
+    clientSecret: process.env.GITHUB_SECRET!,
+  })],
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
