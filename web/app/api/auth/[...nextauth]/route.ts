b21c424d00b8161f85dfa3c41b4ef626a30ff005
anurag5764 <danurag983@gmail.com>
2025-06-27 18:21:25 +0530

    route is updated

--- web/app/api/auth/[...nextauth]/route.ts
+++ web/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-export const authOptions = {
-  providers: [GitHub({
-    clientId: process.env.GITHUB_ID!,
-    clientSecret: process.env.GITHUB_SECRET!,
-  })],
-};
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+
+export const { handlers: { GET, POST } } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+});
