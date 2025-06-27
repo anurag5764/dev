c2baa513f4ce257d934472e714d14914e565967a
anurag5764 <danurag983@gmail.com>
2025-06-27 18:35:39 +0530

    1

--- web/app/api/auth/[...nextauth]/route.ts
+++ web/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,13 @@
-import NextAuth from "next-auth";
-import { AuthOptions } from "next-auth";
-import GitHub from "next-auth/providers/github";
-export const authOptions: AuthOptions = {
-  providers: [GitHub({
-    clientId: process.env.GITHUB_ID!,
-    clientSecret: process.env.GITHUB_SECRET!,
-  })],
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+
+const authConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
 };
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+
+export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth(authConfig);
